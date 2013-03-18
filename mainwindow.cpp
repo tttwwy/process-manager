0d0d25b9b39bbb6048a9c1e319f378fe7627e8d9
= <=>
2013-03-18 11:34:26 +0800

    界面小修改

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+ui->treeWidget->setSortingEnabled(true);
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -22,6 +22,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
 
 };
 
